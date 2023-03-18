58276bd80cdc90527875f866c02e7817e4cb9ff4
Luan Braz <luan_silvacpv@hotmail.com>
2023-03-18 18:29:59 -0300

    Tipos Enumerados

---
+++ src/main/java/com/luan/helpdesk/enums/Perfil.java
@@ -0,0 +1,34 @@
+package com.luan.helpdesk.enums;
+
+public enum Perfil {
+
+    ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "CLIENTE"), TECNICO(2, "TECNICO");
+
+    private Integer codigo;
+    private  String descricao;
+
+    private Perfil(Integer codigo, String descricao){
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Perfil toEnum(Integer cod){
+        if (cod == null){
+            return null;
+        }
+        for (Perfil x : Perfil.values()){
+            if (cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Perfil inválido");
+    }
+}
