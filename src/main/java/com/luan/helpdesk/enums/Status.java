381f22552f94afa1578f5c44a021c672c1042c04
Luan Braz <luan_silvacpv@hotmail.com>
2023-03-19 19:17:13 -0300

    Classes de dominio iniciadas

--- src/main/java/com/luan/helpdesk/enums/Status.java
+++ src/main/java/com/luan/helpdesk/enums/Status.java
@@ -2,7 +2,7 @@ package com.luan.helpdesk.enums;
 
 public enum Status {
 
-    ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+    ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), FECHADO(2, "FECHADO");
 
     private Integer codigo;
     private  String descricao;
